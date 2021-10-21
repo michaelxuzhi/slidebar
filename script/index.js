9f493d6f570b1bc6a6d64aa4ed09637a3edcde14
michaelxuzhi <michaelxuzhi@foxmail.com>
2021-10-22 01:21:28 +0800

    #提交

---
+++ script/index.js
@@ -0,0 +1,10 @@
+let btn_tips = document.querySelector('.tips');
+btn_tips.addEventListener('mouseover', appear);
+btn_tips.addEventListener('mouseout', disappear);
+
+// function appear(e) {
+//   btn_tips.style.left = 0 + 'px';
+// }
+// function disappear(e) {
+//   btn_tips.style.left = -150 + 'px';
+// }
